fb3a4f92e461466f8aea13c9cd7cf3c9d5530406
Martin Janousek <mjanousek92@gmail.com>
2016-10-28 18:10:43 +0200

    non working version

--- src/bms1A.c
+++ src/bms1A.c
@@ -8,8 +8,8 @@
 #include "ecc.h"
 #include "constants.h"
 
-void encode_word(unsigned char *input_buffer, unsigned char *code_word) {
-    encode_data(input_buffer, DATA_BYTES, code_word);
+void encode_word(unsigned char *input_buffer, unsigned char *code_word, int size) {
+    encode_data(input_buffer, size, code_word);
 }
 
 void print_to_output(unsigned char **interlaced_result, FILE *fout, int words_count, int last_word_byte){
@@ -18,43 +18,43 @@ void print_to_output(unsigned char **interlaced_result, FILE *fout, int words_co
         for (j = 0; j < words_count; ++j) {
             if(j < words_count - 1 || i < last_word_byte){
                 fputc(interlaced_result[j][i], fout);
+                printf("%d%d\n", j, i);
             }
         }
     }
 }
 
+
 unsigned char** interleaving(unsigned char **interlaced_result, unsigned char *code_word, int bytes_read, int words_count){
     if(words_count == 0){
         interlaced_result = malloc(sizeof(unsigned char*));
-        printf("prvni\n");
-        interlaced_result = realloc(interlaced_result, sizeof(unsigned char*) * 2);
     } else {
-        printf("druhy\n");
         interlaced_result = realloc(interlaced_result, sizeof(unsigned char*) * (words_count + 1));
     }
     interlaced_result[words_count] = malloc(bytes_read * sizeof(unsigned char*));
-
     memcpy(interlaced_result[words_count], code_word, bytes_read);
+    printf("INTE: %s", interlaced_result[words_count]);
     return interlaced_result;
 }
 
 void add_to_output(unsigned char *code_word, FILE *fout, int size){
     fwrite(code_word, sizeof(unsigned char), size, fout);
 }
-//
-//void create_output_file_name(char *output_file_name, char *input_file_name) {
-//    output_file_name = malloc(strlen(input_file_name)+1+4);
-//    strcpy(output_file_name, input_file_name);
-//    strcat(output_file_name, ".out");
-//}
+
+char * create_output_file_name(char *output_file_name, char *input_file_name) {
+    output_file_name = malloc(strlen(input_file_name)+1+4);
+    strcpy(output_file_name, input_file_name);
+    strcat(output_file_name, ".out");
+    return output_file_name;
+}
 
 int main(int argc, char *argv[]) {
 
-    int bytes_read, words_count = 0;
-    char *file_name/*, *file_name_out = ""*/;
+    int bytes_read, words_count = 0, last_word_bytes = 0;
+    char *file_name, *file_name_out = "";
     FILE *fin;
     FILE *fout;
-    unsigned char input_buffer[DATA_BYTES];
+    unsigned char input_buffer[DATA_BYTES + 1];
     unsigned char code_word[DATA_BYTES+NPAR];
 
     unsigned char **interlaced_result;
@@ -68,21 +68,25 @@ int main(int argc, char *argv[]) {
     }
 
     file_name = argv[1];
-//    create_output_file_name(file_name_out, file_name);
+    file_name_out = create_output_file_name(file_name_out, file_name);
     fin = fopen(file_name , "r");
-    fout = fopen("input.out", "w");
+    fout = fopen(file_name_out, "w");
+
 
     if(fin == NULL) {
         fprintf(stderr, "Error opening file\n");
         return EXIT_FAILURE;
     }
 
-    memset(input_buffer, 0, sizeof(unsigned char) * (DATA_BYTES));
+    memset(input_buffer, 0, sizeof(unsigned char) * (DATA_BYTES + 1));
     memset(code_word, 0, sizeof(unsigned char) * (DATA_BYTES + NPAR));
     while((bytes_read = fread(input_buffer, sizeof(unsigned char), DATA_BYTES, fin)) > 0){
-        printf("Ctu slovo\n");
+        printf("READ: \"%s\"\n", input_buffer);
         //zakodovani
-        encode_word(input_buffer, code_word);
+        encode_word(input_buffer, code_word, bytes_read);
+
+        printf("ENCO: \"%s\"\n", code_word);
+
         //TODO prokladani
         interlaced_result = interleaving(interlaced_result, code_word, bytes_read + NPAR, words_count);
 
@@ -90,10 +94,12 @@ int main(int argc, char *argv[]) {
         memset(input_buffer, 0, sizeof(unsigned char) * (DATA_BYTES));
         memset(code_word, 0, sizeof(unsigned char) * (DATA_BYTES + NPAR));
         ++words_count;
+        last_word_bytes = bytes_read;
     }
 
     //tisk do vystupu
-    print_to_output(interlaced_result, fout, words_count, bytes_read + NPAR);
+    printf("-%d-", last_word_bytes + NPAR);
+    print_to_output(interlaced_result, fout, words_count, last_word_bytes + NPAR);
 
     fclose(fin);
     fclose(fout);
--- src/bms1B.c
+++ src/bms1B.c
@@ -33,10 +33,11 @@ void add_to_output(unsigned char *code_word, FILE *fout, int size){
     fwrite(code_word, sizeof(unsigned char), size, fout);
 }
 
-void create_output_file_name(char *output_file_name, char *input_file_name) {
+char * create_output_file_name(char *output_file_name, char *input_file_name) {
     output_file_name = malloc(strlen(input_file_name)+1+3);
     strcpy(output_file_name, input_file_name);
     strcat(output_file_name, ".ok");
+    return output_file_name;
 }
 
 int main(int argc, char *argv[]) {
@@ -46,7 +47,6 @@ int main(int argc, char *argv[]) {
     FILE *fin;
     FILE *fout;
     unsigned char decoded_code_word[DATA_BYTES + 1];
-
 
     /* Initialization the ECC library */
     initialize_ecc ();
@@ -58,7 +58,8 @@ int main(int argc, char *argv[]) {
 
     file_name = argv[1];
     fin = fopen(file_name , "r");
-    fout = fopen("input.out.ok", "w");
+    file_name_out = create_output_file_name(file_name_out, file_name);
+    fout = fopen(file_name_out, "w");
 
     if(fin == NULL) {
         fprintf(stderr, "Error opening file\n");
@@ -67,25 +68,29 @@ int main(int argc, char *argv[]) {
 
     fseek(fin, 0L, SEEK_END);
     int sizeOfFile = ftell(fin);
+    printf("size of file: %d\n", sizeOfFile);
     //seeking to go to the beginning)
     rewind(fin);
     int word_count = ceil(((float) sizeOfFile) / (DATA_BYTES+NPAR));
     int last_word_bytes = sizeOfFile % (DATA_BYTES+NPAR);
     unsigned char coded_file[word_count][DATA_BYTES+NPAR];
     unsigned char code_interlaced_word[word_count];
+    last_word_bytes = (last_word_bytes == 0) ? DATA_BYTES : last_word_bytes;
 
 
     int i = 0;
     int word_lenght = word_count;
+    printf("Number of words %d\n", word_lenght);
     while((bytes_read = fread(code_interlaced_word, sizeof(unsigned char), word_lenght, fin)) > 0) {
-        int j;
-        for (j = 0; j < bytes_read; ++j) {
+        printf("size of file: %d\n", sizeOfFile);
+        for (int j = 0; j < bytes_read; ++j) {
             coded_file[j][i] = code_interlaced_word[j];
         }
         i++;
         if(i == last_word_bytes) {
             word_lenght--;
         }
+
     }
 
 //    memset(decoded_code_word, 0, (DATA_BYTES + 1) * sizeof(char));
@@ -94,19 +99,23 @@ int main(int argc, char *argv[]) {
     for (i = 0; i < word_count; ++i) {
         if(i == word_count - 1){
             bytes_count = last_word_bytes;
+            printf("Last word: %d\n", last_word_bytes);
         }
         //TODO prokladani
         //zakodovani
+        printf("ENCO: %s\n", coded_file[i]);
         decode_data(coded_file[i], bytes_count);
         //TODO correct errors
         if (check_syndrome() != 0) {
             correct_errors_erasures(coded_file[i], bytes_count, 0, NULL);
             printf("Corrected codeword: \"%s\"\n",  coded_file[i]);
         }
+        printf("DECO: %s\n", coded_file[i]);
         memcpy(decoded_code_word, coded_file[i], bytes_count - NPAR);
-        decoded_code_word[bytes_count - NPAR] = '\0';
+        printf("DECO: %s\n", decoded_code_word);
+//        decoded_code_word[bytes_count - NPAR] = '\0';
         //tisk do vystupu
-        add_to_output(decoded_code_word, fout, bytes_count - NPAR + 1);
+        add_to_output(decoded_code_word, fout, bytes_count - NPAR);
 
         //vycisteni pameti
         memset(decoded_code_word, 0, (DATA_BYTES) * sizeof(char));
